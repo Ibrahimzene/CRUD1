1fd7286fe5d2eaf9858af9b14a9afc7a8e1f9331
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-08 14:12:13 +0100

    Add flashcardHandlers and update db.json

---
+++ backend/api/handles/flashcardHandlers.ts
@@ -0,0 +1,11 @@
+import { getDb } from './dbtools';
+
+const db = await getDb();
+
+export const getAllFlashcards = () => {
+	return db.data.flashcards;
+}
+
+export const getOneFlashcard = (suuid: string) => {
+
+}
