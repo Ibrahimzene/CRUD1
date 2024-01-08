c919e4e2cd4543b6513f6175bfef33edf922f263
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-08 16:05:24 +0100

    Fix import path in dbtools.ts and add missing types in flashcardrouter.ts

--- backend/api/routers/flashcardrouter.ts
+++ backend/api/routers/flashcardrouter.ts
@@ -1,39 +1,19 @@
-import { Router } from 'express';
-import * as flashcardHandlers from '../handlers/flashcardHandlers';
+export interface INewFlashcard {
+	category: string;
+	front: string
+	back: string;
+}
 
-export const flashcardRouter = Router();
+export interface IFlashcard extends INewFlashcard {
+	suuid: string;
+}
 
-flashcardRouter.get('/', (_req, res) => {
-	const flashcards = flashcardHandlers.getAllFlashcards();
-	res.json(flashcards);
-});
+export interface IPatchFlashcard {
+	category?: string;
+	front?: string
+	back?: string;
+}
 
-flashcardRouter.get('/:suuid', (req, res) => {
-	const suuid = req.params.suuid;
-	const flashcard = flashcardHandlers.getOneFlashcard(suuid);
-	if (flashcard) {
-		res.json(flashcard);
-	} else {
-		res.status(404).json(`Flashcard with suuid "${suuid}" not found.`)
-	}
-});
-
-flashcardRouter.post('/', (req, res) => {
-	const newFlashcard = req.body;
-	console.log('body', newFlashcard);
-	res.json('testing...');
-});
-
-flashcardRouter.put('/:suuid', (req, res) => {
-	const suuid = req.params.suuid;
-	res.json(`replace all fields flashcard with suuid ${suuid}`);
-});
-
-flashcardRouter.patch('/:suuid', (req, res) => {../.
-	res.json(`replace some fields on flashcard with suuid ${suuid}`);
-});
-
-flashcardRouter.delete('/:suuid', (req, res) => {
-	const suuid = req.params.suuid;
-	res.json(`delete flashcard with suuid ${suuid}`);
-});
+export interface IDatabase {
+	flashcards: IFlashcard[];
+}
